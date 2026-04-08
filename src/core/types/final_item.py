dcbd72c4c6e770b013912f95544ed25d536249f3
CesarManzoCode <cesarmanzocode@gmail.com>
2026-04-07 22:50:08 -0600

    class FinalItem created

---
+++ src/core/types/final_item.py
@@ -0,0 +1,12 @@
+from dataclasses import dataclass
+from typing import List
+
+
+@dataclass
+class FinalItem:
+    title: str
+    summary: str
+    key_points: List[str]
+    details: str
+    source: str
+    url: str
